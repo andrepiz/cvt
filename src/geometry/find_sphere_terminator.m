3a20f53090c57d05cdd4576be76b4d84f7c3602c
andrepiz <andpiz1997@gmail.com>
2024-12-06 14:59:39 +0100

    [UPG] Upgraded find_sphere_slice function to return also before and after indexes

--- src/geometry/find_sphere_terminator.m
+++ src/geometry/find_sphere_terminator.m
@@ -1,8 +1,8 @@
-function ixs = find_sphere_terminator(lon, lat, span, k, method)
+function [ixs, ixs_before, ixs_after] = find_sphere_terminator(lon, lat, span, k, method)
 %FIND_SPHERE_TERMINATOR Find the longitude and latitude sectors that belongs to
 %the terminator of the sphere and within a given span angle. The parameter k tunes the reduction
 %in the terminator size when approaching the poles.
 
-ixs = find_sphere_slice(lon, lat, 0, span, k, method);
+[ixs, ixs_before, ixs_after] = find_sphere_slice(lon, lat, 0, span, k, method);
 
 end
