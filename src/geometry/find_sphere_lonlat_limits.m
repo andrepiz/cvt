65b4205dda581e46818630a291a4db52d14f3fd2
andrepiz <andpiz1997@gmail.com>
2025-02-26 12:19:44 +0100

    [FIX] Simplified lonlat limits algo

--- src/geometry/find_sphere_lonlat_limits.m
+++ src/geometry/find_sphere_lonlat_limits.m
@@ -13,7 +13,7 @@ end
 if tol == 0
     error('Tolerance must be larger than 0')
 end
-nper = max(1, 20*ceil(1/tol));
+nper = min(max(1, 20*ceil(1/tol)), 100e3);
 
 err = tol;
 lon_lims = pi/2*[-1 1];
@@ -28,8 +28,7 @@ while err >= tol
     nper = round(nper*1.1);
 
     % Computing intersections
-    [P_inter_TAR, los_inter_TAR] = intersect_fov_sphere(pos_c2t_TAR, dcm_TAR2CAM, R, fov, nper);
-
+    [P_inter_TAR, los_inter_TAR] = intersect_fov_sphere(pos_c2t_TAR, dcm_TAR2CAM, R, fov, nper, false);
     % Correcting number of points
     nper = size(P_inter_TAR, 2);
 
@@ -46,6 +45,11 @@ while err >= tol
     end
 end
 
+% keep only the consecutive intersections
+ixs_nonan = all(~isnan(P_inter_TAR));            % find nans
+ixs_isolated_nonan = ixs_nonan & ~[ixs_nonan(2:end), ixs_nonan(1)];
+P_inter_TAR(:, ixs_isolated_nonan) = [];
+
 ninter = sum(~all(isnan(P_inter_TAR)));
 if flag_debug
     fprintf(['\n+++ ',num2str(ninter), ' intersections found out of ', num2str(nper), '!'])
@@ -57,16 +61,16 @@ bsDir_TAR = dcm_TAR2CAM'*[0;0;1];
 d_c2t = norm(pos_c2t_TAR);
 dir_c2t_TAR = pos_c2t_TAR./d_c2t;
 phase_angle = atan2(-dir_c2t_TAR(2),-dir_c2t_TAR(1));
-[phi_tangency, br_tangency] = find_sphere_tangent_angle(d_c2t, R);
+[hs_tangency, br_tangency] = find_sphere_tangent_angle(d_c2t, R);
 
 if ninter == 0
-    % No intersections found means either the FOV do not cross at all or it entirely
+    % Zero intersections found means either the FOV do not cross at all or it entirely
     % contains the sphere. To understand which situation we are, we check 
     % the angles of the FOV LOS projected on the cam-target plane with 
     % respect to boresight.
 
     flag_in_fov = false;
-    if dot(bsDir_TAR, dir_c2t_TAR) - 1 <= eps
+    if abs(dot(bsDir_TAR, dir_c2t_TAR) - 1) <= eps
         % Boresight and target direction are aligned
         flag_in_fov = true;
     else
@@ -79,8 +83,8 @@ if ninter == 0
         end
     end
     if flag_in_fov
-        lon_lims = phase_angle + phi_tangency*[-1 1];
-        lat_lims = phi_tangency*[-1 1];
+        lon_lims = phase_angle + hs_tangency*[-1 1];
+        lat_lims = hs_tangency*[-1 1];
     end    
 
 elseif ninter ~= nper
@@ -89,28 +93,11 @@ elseif ninter ~= nper
     % Rotate to a REF frame where X is aligned with dir_t2c
     dcm_TAR2REF = euler_to_dcm([0; 0; phase_angle]);
     P_inter_REF = dcm_TAR2REF*P_inter_TAR;
-    bsDir_REF = dcm_TAR2REF*bsDir_TAR;
-
-    % Find direction of filling
-    ixs_nan = all(isnan(P_inter_REF));
-    %ix_first = find(~ixs_nan, 1, 'first');
-    %ix_last = find(~ixs_nan, 1, 'last');
-    ixs_first_nans = find(diff([0, ixs_nan])>0);
-    ixs_last_nans = find(diff([ixs_nan, 0])<0);
-    thetaFirst = atan2(P_inter_REF(3,ixs_first_nans(1)), P_inter_REF(2, ixs_first_nans(1)));
-    thetaLast = atan2(P_inter_REF(3,ixs_last_nans(end)), P_inter_REF(2, ixs_last_nans(end)));
-    thetaBs = atan2(bsDir_REF(3), bsDir_REF(2));
-    if thetaLast > thetaFirst
-        dir_filling = thetaBs > thetaLast | thetaBs < thetaFirst; % clockwise 
-        dir_filling = +1*dir_filling -1*~dir_filling;
-    else
-        dir_filling = thetaBs > thetaFirst | thetaBs < thetaLast; % anticlockwise 
-        dir_filling = +1*dir_filling -1*~dir_filling;
-    end        
 
     % Fill with circles the missing points using the correct direction
+    dir_filling = -1;
     [P_inter_REF([2 3],:), ixs_nan] = fill_nans_with_arcs(P_inter_REF([2 3],:), dir_filling, flag_debug);
-    P_inter_REF(1, ixs_nan) = R*cos(phi_tangency); 
+    P_inter_REF(1, ixs_nan) = R*cos(hs_tangency); 
 
     % Rotate back to TAR frame
     P_inter_TAR = dcm_TAR2REF'*P_inter_REF;
@@ -127,7 +114,7 @@ end
 if flag_debug
     k1 = R;
     k2 = R;
-    k3 = R;
+    k3 = 0.5*(d_c2t - R);
     nper = 100;
     [x,y,z] = sphere(100);
     figure('units','normalized','position',[0.05, 0.05, 0.8, 0.8]); 
@@ -136,7 +123,7 @@ if flag_debug
     surf(k1*x, k1*y, k1*z, 'EdgeColor','k','FaceColor','k','FaceAlpha',0.2,'EdgeAlpha',0.3);
     plot3(P_inter_TAR(1,:), P_inter_TAR(2,:), P_inter_TAR(3,:),'k-','LineWidth',2)
     try
-        plot3(P_inter_TAR(1, ixs_nan), P_inter_TAR(2, ixs_nan), P_inter_TAR(3, ixs_nan),'g-','LineWidth',2)
+        plot3(P_inter_TAR(1, ixs_nan), P_inter_TAR(2, ixs_nan), P_inter_TAR(3, ixs_nan),'g.','LineWidth',2)
     catch
     end
     scatter3(P_lims(1,:), P_lims(2,:), P_lims(3,:), 100,'b*','LineWidth',2)
